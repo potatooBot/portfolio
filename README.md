bfa757824ab1f9a035b7f6915c189ba4e8c60202
Keshav Kumar <kumarkeshav825@gmail.com>
2022-09-29 23:47:46 +0530

    terminal added

---
+++ README.md
@@ -0,0 +1,1 @@
+# techsouvik.github.io
---
+++ js/commands.js
@@ -0,0 +1,60 @@
+var instagram ="https://instagram.com/ee.fffff";
+var github = "https://github.com/potatooBot/";
+var email = 'mailto:kumarkeshav825@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Keshav!👋",
+  "I'm a software developer.",
+  "I am currently a pre-final year student, ",
+  "pursuing Electronics and Communication Engineering from IEM Kolkata.",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "You tell me :/",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'instagram       <a href="' + instagram + '" target="_blank">instagram.com/ee.fffff' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/potatooBot' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          (Having an existential crisis right now)',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Email Me!',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+//   '  _____                               _____        __    __                    ____          __   __           ',
+//  '  / ___/ __  __ _____ __  __ ____ _   / ___/ ___   / /__ / /_   ____ _ _____   / __ \ ____ _ / /_ / /_ ____ _  ',
+//  '  \__ \ / / / // ___// / / // __ `/   \__ \ / _ \ / //_// __ \ / __ `// ___/  / / / // __ `// __// __// __ `/  ',
+//  '  ___/ // /_/ // /   / /_/ // /_/ /   ___/ //  __// ,<  / / / // /_/ // /     / /_/ // /_/ // /_ / /_ / /_/ /  ',
+//  ' /____/ \__,_//_/    \__, / \__,_/   /____/ \___//_/|_|/_/ /_/ \__,_//_/     /_____/ \__,_/ \__/ \__/ \__,_/   ',
+//    '                 /____/           ',                                                                         
+  '<span class="color2">Welcome to my web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
